9f2926d0950b85c6976006569c58371f386c9f86
matjaklol <129894843+matjaklol@users.noreply.github.com>
2025-04-14 20:15:23 -0600

    Update StreakCommand.kt to properly show next streak reward
    Literally a single line that fixes the amount displayed when running /streak to cap the value at $50 (the current value given to players daily after the minimum time).
    
    Also added some comments to make it slightly more readable, Kotlin is still balls though.
    
    -keyboard

--- src/main/java/dev/shog/osmpl/util/commands/StreakCommand.kt
+++ src/main/java/dev/shog/osmpl/util/commands/StreakCommand.kt
@@ -11,18 +11,23 @@ import java.util.concurrent.TimeUnit
  * The streak  command.
  */
 internal val STREAK_COMMAND = Command.make("streak") {
+    //Query db for streak info based on the player username
     val rs = SqlHandler.getConnection(db = "money")
             .prepareStatement("SELECT lastLogin, streak FROM dailyreward WHERE player = ?")
             .apply {
                 setString(1, sender.name.toLowerCase())
             }
             .executeQuery()
-
+            
+    //if streak info is found send info to player
     if (rs.next()) {
+        
+        //"Currently at a login streak of XX Days, next reward is $XX*5. In XXtimer"
+        //Since reward is capped at $50 just replace that info to max out at $50.
         sendMessageHandler(
                 "streak.command.default",
                 rs.getInt("streak"),
-                rs.getInt("streak") * 5,
+                if (rs.getInt("streak") * 5 > 50) 50 else rs.getInt("streak") * 5,
                 ((TimeUnit.DAYS.toMillis(1) + rs.getLong("lastLogin")) - System.currentTimeMillis()).fancyDate() // not readable lol
         )
     } else {
